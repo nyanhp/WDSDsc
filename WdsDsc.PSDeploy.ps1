cf6ebe8abf4efc309db7f746e29699c30e2e486b
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-07-15 15:30:59 +0200

    Update AppVeyor.yml

--- WdsDsc.PSDeploy.ps1
+++ WdsDsc.PSDeploy.ps1
@@ -1,19 +1,34 @@
-if ($env:BHBuildSystem -ne 'Unknown' -and $ENV:BHBranchName -eq "master") {
-Deploy Module {
+if ($env:BHBuildSystem -ne 'Unknown' -and $ENV:BHBranchName -eq "master")
+{
+    Deploy Module {
         By PSGalleryModule {
-            FromSource $env:BHProjectName
+            FromSource (Join-Path $ENV:BHProjectPath $ENV:BHProjectName)
             To PowerShell
             WithOptions @{
                 ApiKey = $env:NugetApiKey
-                Force = $true
+                Force  = $true
             }
         }
     }
 }
-else {
+else
+{
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
     "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
-    "`t* Module path is valid (Current: )" |
-        Write-Host
+    "`t* Module path is valid (Current: $(Join-Path $ENV:BHProjectPath $ENV:BHProjectName))" |
+    Write-Host
+}
+
+if ((Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -and $env:BHBuildSystem -eq 'AppVeyor')
+{
+    Deploy DeveloperBuild {
+        By AppVeyorModule {
+            FromSource (Join-Path $ENV:BHProjectPath $ENV:BHProjectName)
+            To AppVeyor
+            WithOptions @{
+                Version = $env:APPVEYOR_BUILD_VERSION
+            }
+        }
+    }
 }
